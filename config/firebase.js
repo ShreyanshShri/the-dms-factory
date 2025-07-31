8833fd326f258b9b53055f7328c941c66d944118
Shreyansh Shrivastva <shreyanshshrivastva@gmail.com>
2025-07-31 14:34:15 +0530

    Testing...

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,18 @@
-const admin = require("firebase-admin");
+// const admin = require("firebase-admin");
 
-// Initialize Firebase Admin with your service account key
-const serviceAccount = require("./firebase-service-account.json");
+// // Initialize Firebase Admin with your service account key
+// const serviceAccount = require("./firebase-service-account.json");
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://cold-dms.firebaseio.com", // optional unless using Realtime DB
-});
+// admin.initializeApp({
+// 	credential: admin.credential.cert(serviceAccount),
+// 	databaseURL: "https://cold-dms.firebaseio.com", // optional unless using Realtime DB
+// });
 
-const db = admin.firestore();
+// const db = admin.firestore();
+
+// module.exports = { admin, db };
+
+const admin = "";
+const db = "";
 
 module.exports = { admin, db };
