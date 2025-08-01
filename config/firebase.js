58fffe10743f2ddcc290304d6fa3806f7e27d30e
Shreyansh Shrivastva <shreyanshshrivastva@gmail.com>
2025-08-01 12:44:06 +0530

    Fixed firebase auth

--- config/firebase.js
+++ config/firebase.js
@@ -1,16 +1,10 @@
 const admin = require("firebase-admin");
 
 // Initialize Firebase Admin with your service account key
-// const serviceAccount = require("./firebase-service-account.json");
-
-const credential = {
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-	privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-};
+const serviceAccount = require("./firebase-service-account.json");
 
 admin.initializeApp({
-	credential: admin.credential.cert(credential),
+	credential: admin.credential.cert(serviceAccount),
 	databaseURL: "https://cold-dms.firebaseio.com", // optional unless using Realtime DB
 });
 
